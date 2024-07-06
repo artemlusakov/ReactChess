3cb0978aa2d63814ce0ca61dc5f9ce07aae42f06
zero <artemlysakov793@gmail.com>
2024-07-06 21:37:39 +0300

    AddProjectChess
    Необходимо добавить логику хода короля

---
+++ Chess/src/main.tsx
@@ -0,0 +1,10 @@
+import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
